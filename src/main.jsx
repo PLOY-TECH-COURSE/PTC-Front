26a357fe8f0fe2c4c31c5acc116a03b56e983ea0
123isi <123isi@naver.com>
2025-01-23 12:31:49 +0900

    feat :: :ㅈㅂ
    wq
    q
    ;

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { createRoot } from 'react-dom/client'
+import { useNavigate } from 'react-router-dom';
+
 import {
     BrowserRouter,
     Routes,
     Route
 } from "react-router-dom";
 import Onboarding from './pages/onboarding.jsx';
-
+import Login from './pages/login.jsx';
+import Signup from './pages/signup.jsx';
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Onboarding />} />
+            <Route path='/' element={<Onboarding />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     </BrowserRouter>
 )
