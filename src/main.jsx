1df231b1dab500737a5fcac1a482ae43851ff503
soeun823 <bsoeun866@gmail.com>
2025-01-24 01:44:19 +0900

    docs(#23) :: main 충돌 수정

--- src/main.jsx
+++ src/main.jsx
@@ -10,24 +10,17 @@ import {RecoilRoot} from 'recoil';
 
 import Onboarding from './pages/onboarding/index.jsx';
 import Apply from './pages/apply/index.jsx';
-<<<<<<< HEAD
-import Proposer from './pages/proposer/index.jsx';
-import Login from './pages/login.jsx';
-import Signup from './pages/signup.jsx';
-=======
 import Login from './pages/login/login.jsx';
 import Signup from './pages/signup/signup.jsx';
->>>>>>> origin/main
 
 createRoot(document.getElementById('root')).render(
-<RecoilRoot>
+   <RecoilRoot>
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Onboarding />} />
             <Route path="/apply" element={<Apply/>} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/proposer" element={<Proposer />} />
         </Routes>
     </BrowserRouter>
 </RecoilRoot>
