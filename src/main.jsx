63f3fd92d7d78ed7edbcf43f2cba9173f1899a50
soeun823 <bsoeun866@gmail.com>
2025-01-23 16:40:25 +0900

    fix(#23) :: 충돌

--- src/main.jsx
+++ src/main.jsx
@@ -11,25 +11,19 @@ import {RecoilRoot} from 'recoil';
 
 import Onboarding from './pages/onboarding/index.jsx';
 import Apply from './pages/apply/index.jsx';
-<<<<<<< Updated upstream
-=======
 import Proposer from './pages/proposer/index.jsx';
 import Login from './pages/login.jsx';
 import Signup from './pages/signup.jsx';
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
-   <RecoilRoot>
+<RecoilRoot>
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Onboarding />} />
             <Route path="/apply" element={<Apply/>} />
-<<<<<<< Updated upstream
-=======
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/proposer" element={<Proposer />} />
->>>>>>> Stashed changes
         </Routes>
     </BrowserRouter>
 </RecoilRoot>
