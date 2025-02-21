a0a4054a778016dede364199251c063760fa2806
whdkfk <joara080827@gmail.com>
2025-02-21 17:47:05 +0900

    feat(#41) :: 글 목록 리스트 구현

--- src/pages/PostList/style.jsx
+++ src/pages/PostList/style.jsx
@@ -6,12 +6,14 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     width: 100%;
+    align-items: center;
+    justify-content: centers;
+    overflow-y: auto;
 `
 
 export const PostListTop = styled.div`
     width: 100%;
     height: 50%;
-    justify-content: center;
     display: flex;
     margin-top: 50px;
     flex-direction: column;
@@ -28,7 +30,6 @@ export const Search = styled.div`
     align-items: center;
     padding: 1%;
     border-radius: 13px;
-    margin-left: 100px;
 `
 
 export const Input = styled.input`
@@ -41,7 +42,7 @@ export const Sort = styled.div`
     display: flex;
     gap: 20px;
     margin-top: 30px;
-    margin-left: 450px;
+    margin-left: 410px;
 `
 
 export const Recent = styled.div`
@@ -82,5 +83,125 @@ export const Like = styled.div`
     p{
         font-weight: 100;
         opacity: 0.78;
+    }
+`
+
+export const PostListMain = styled.div`
+    width: 100%;
+    padding: 3% 5%;
+    display: flex;
+    flex-wrap: wrap;
+    height: 70vh;
+    justify-content: space-between;
+    row-gap: 40px;
+`
+
+export const PostData = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+`
+export const RowData = styled.div`
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #cccccc;
+    width: 27.5vw;
+    height: 25vh;
+    border-radius: 10px;
+    min-width: 27vw;
+    padding: 2% 1.3%;
+    gap: 10px;
+    cursor: pointer;
+`
+
+export const PostImg = styled.div`
+    width: 40%;
+    height: 100px;
+    margin-right: 10px;
+    align-items: center;
+    justify-content: center;
+
+    img{
+        width: 100%;
+        object-fit: cover;
+    }
+`
+
+export const PostRightData = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    
+    span{
+        border: 0.5px solid #D1D1D1;
+        margin-top: 60px;
+    }
+`
+
+export const PostRightTopData = styled.div`
+    display: flex;
+    flex-direction: column; 
+    width: 100%;
+    gap: 10px;
+
+    p{
+        font-size: 10px;
+        color: gray;
+    }
+`
+
+export const PostRightBottomData = styled.div`
+    display: flex;
+    width: 100%;
+    gap: 10px;
+    align-items: center;
+    width: 200px;
+    margin-top: 10px;
+    margin-left: 5px;
+`
+
+
+export const PostProfile = styled.div`
+
+    img{
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+    }
+`
+
+export const PostbottomData = styled.div`
+
+    span{
+        font-size: 10px;
+        color: #D1D1D1;
+        border: none;
+    }
+`
+
+export const Name = styled.p`
+    font-size: 13px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-weight: 500;
+    cursor: pointer;
+`
+
+export const PostLike = styled.div`
+    display: flex;
+    justify-content: centers;
+    align-items: center;
+    gap: 5px;
+    margin-left: 30px;
+    cursor: pointer;
+
+    img{
+        width: 15px;
+    }
+
+    p{
+        font-size: 13px;
+        font-family: 'Courier New', Courier, monospace;
+        font-weight: 600;
+        margin-top: 2px;
     }
 `
