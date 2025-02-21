79b1318b3b027dfb13b9b341a983e46b24582d36
whdkfk <joara080827@gmail.com>
2025-02-21 22:15:05 +0900

    파일 수정

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,8 @@ import PostList from './pages/PostList/index.jsx';
 import Write from './pages/write/index.jsx';
 import MyPage from './pages/mypage/mypage.jsx';
 import Detail from './pages/postList/detail/index.jsx';
+import Broadcast from './pages/broadcast/index.jsx';
+import Detail2 from './pages/broadcast/detail/index.jsx';
 
 createRoot(document.getElementById('root')).render(
    <RecoilRoot>
@@ -30,6 +32,8 @@ createRoot(document.getElementById('root')).render(
             <Route path='/mypage' element={<MyPage />} />
             <Route path='/write' element={<Write />} />
             <Route path="/post/:id" element={<Detail />} /> 
+            <Route path='/broadcast' element={<Broadcast />} />
+            <Route path="/announcement/:id" element={<Detail2 />} /> 
         </Routes>
     </BrowserRouter>
 </RecoilRoot>
