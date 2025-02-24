c0a60edd1d19067eebf056dec5bb70c44ca4aef4
whdkfk <joara080827@gmail.com>
2025-02-24 22:12:10 +0900

    refactor(#41) :: 글 검색 에러 수정

--- src/pages/PostList/index.jsx
+++ src/pages/PostList/index.jsx
@@ -68,10 +68,10 @@ export default function () {
                 </S.PostListTop>
 
                 <S.Sort>
-                    <S.Recent onClick={() => handleSortChange("CREAT_AT")} active={sort === "LIKE"}>
+                    <S.Recent onClick={() => handleSortChange("CREATE_AT")} active={sort === "LIKE"}>
                         <button /><p>최신순</p>
                     </S.Recent>
-                    <S.Like onClick={() => handleSortChange("LIKE")} active={sort === "CREAT_AT"}>
+                    <S.Like onClick={() => handleSortChange("LIKE")} active={sort === "CREATE_AT"}>
                         <button /><p>좋아요순</p>
                     </S.Like>
                 </S.Sort>
