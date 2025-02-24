a72569c852986e099f194cc113fdcd4852e6aea2
이효준 <148470980+123isi@users.noreply.github.com>
2025-02-25 01:11:45 +0900

    fix(#89) :: 오류 수정

--- src/pages/mypage/mypage.jsx
+++ src/pages/mypage/mypage.jsx
@@ -142,6 +142,16 @@ const Mypage = () => {
     })
     .catch((error) => console.error("API 요청 실패:", error));
   }, [userId]);
+  const handleEditClick = () => {
+    if (isEditing) {
+      setUserData((prev) => ({
+        ...prev,
+        uid: editedUid,
+        bio: editedBio,
+      }));
+    }
+    setIsEditing(!isEditing);
+  };
   
   return (
     <>
