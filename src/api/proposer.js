449b16ad5932bd9bdc013fe6aadf0841154c26e2
soeun823 <bsoeun866@gmail.com>
2025-02-25 16:39:21 +0900

    feat(#38) ::신청자 조회 api 연결

--- src/api/proposer.js
+++ src/api/proposer.js
@@ -2,10 +2,15 @@ import axiosInstance from "../lib/axiosInstance";
 
 export const getProposerList = async () => {
   try {
-    const res = await axiosInstance.get("/proposers");
-    return res.data; // 신청자 리스트 반환
+    const token = localStorage.getItem("accessToken"); // 저장된 토큰 가져오기
+    const res = await axiosInstance.get("/applications", {
+      headers: {
+        Authorization: `Bearer ${token}`, // 토큰 추가
+      },
+    });
+    return res.data;
   } catch (err) {
-    console.error(err);
+    console.error("API 요청 실패:", err);
     return [];
   }
 };
