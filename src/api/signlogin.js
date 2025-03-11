034e4115e08e211b9b6174eb44cd0fa08fcf2e9e
이효준 <148470980+123isi@users.noreply.github.com>
2025-03-11 11:24:11 +0900

    Update signlogin.js

--- src/api/signlogin.js
+++ src/api/signlogin.js
@@ -17,10 +17,6 @@ export const signupData = async (name,id,email,code,password, confirmPassword, t
     if(response.status === 200){
         return true;
     }
-    else if (password.length < 10) {
-        alert("비밀번호는 10자 이상이어야 합니다.");
-        return false;
-    }
     return false;
   } catch (error) {
       console.error('회원가입 오류:', error);
