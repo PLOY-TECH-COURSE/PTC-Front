5ac0a99d33d0e8713f1b938b96f14961ac8e014a
dohun08 <bemik624@gmail.com>
2025-03-26 08:43:38 +0900

    refactor(#291) :: 공지변경

--- src/components/modal/write/index.jsx
+++ src/components/modal/write/index.jsx
@@ -14,7 +14,6 @@ export default function WriteModal({data, title, tag, content, setIsModal}){
     const [intro, setIntro] = useState(data?.introduction || '');
     const [isLoading, setIsLoading] = useState(false);
     const {id}= useParams();
-    console.log(data);
     const navigate = useNavigate();
     const changeFile = async (event) => {
         const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
