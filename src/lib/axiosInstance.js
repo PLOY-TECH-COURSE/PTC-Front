de765872ba040fa9cb99272090363dce823d00d7
123isi <123isi@naver.com>
2025-08-10 12:58:55 +0900

    feat :: 퍼블리시이이

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://ploytechcourse.kro.kr/api',
+    baseURL: '/api',
     headers:{
         'Content-Type': 'application/json'
     },
